327b726c1a29c535e905a37dbbc7c441d781dc94
hitesh <hitesh9398rao@gmail.com>
2025-03-15 19:24:35 +0530

    responsive and use favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Head from "next/head";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
+import { siteData } from "@/data/siteData";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,6 +27,7 @@ export default function RootLayout({ children }) {
         {/* <!-- ========== Start Fonts Style ========== --> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="icon" href={siteData.logo || "/placeholder.svg"} />
         <link
           href="https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,100;0,9..40,200;0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;0,9..40,800;1,9..40,100;1,9..40,200;1,9..40,300;1,9..40,400;1,9..40,500;1,9..40,600&family=Syne:wght@400;500;600&family=Yantramanav:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet" />
